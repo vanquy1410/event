91d037614aa06180b6abc51979a67560eeb34fdb
vanquy1410 <quynguyenpro1410@gmail.com>
2024-07-11 16:56:34 +0700

    fix role

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -4,14 +4,28 @@ import { checkRole } from "@/utils/roles";
 import { setRole } from "./_actions";
 import { SearchUsers } from "./_search-users";
 import { clerkClient } from "@clerk/nextjs/server";
+import Link from "next/link";
+
+// Hàm waitFor để tạo một thời gian chờ
+const waitFor = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default async function AdminDashboard(params: {
   searchParams: { search?: string };
 }) {
   if (!checkRole("admin")) {
-    redirect("/");
+    // Hiển thị thông báo không có quyền truy cập
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="bg-red-100 border border-red-400 text-red-700 px-20 py-10 rounded-lg relative max-w-md mx-auto">
+          <strong className="font-bold">Không có quyền truy cập!<br/></strong>
+          <span className="block sm:inline text-center">Bạn cần quyền admin để xem trang này.</span>
+          <div className="mt-4">
+            <Link href="/" className="inline-block bg-purple-500 text-white py-1 px-2 rounded hover:bg-purple-600 transition-colors duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">Back to Home</Link>
+          </div>
+        </div>
+      </div>
+    );
   }
-
   const query = params.searchParams.search;
 
   // Directly assign the result of getUserList to users without accessing .data
