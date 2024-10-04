7325e5c650e0d0006f04bb903b8bef3ad6115560
s1jming <77600026+nhangian2017@users.noreply.github.com>
2024-10-04 23:29:15 +0700

    update upload document

--- app/api/s3-storage/route.ts
+++ app/api/s3-storage/route.ts
@@ -5,6 +5,7 @@ export async function POST(request: NextRequest) {
   try {
     const formData = await request.formData();
     const file = formData.get('file') as File;
+    const fileType = formData.get('fileType') as string;
 
     if (!file) {
       return NextResponse.json({ error: 'Không có file được cung cấp' }, { status: 400 });
@@ -13,7 +14,7 @@ export async function POST(request: NextRequest) {
     const buffer = await file.arrayBuffer();
     const fileName = file.name;
 
-    const result = await uploadFileToS3(Buffer.from(buffer), fileName);
+    const result = await uploadFileToS3(Buffer.from(buffer), fileName, file.type);
 
     return NextResponse.json({
       message: 'Upload thành công',
