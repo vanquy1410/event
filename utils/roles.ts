382e4438f85444f1f49565c08f8705bcd7c93258
vanquy1410 <quynguyenpro1410@gmail.com>
2024-10-08 11:32:10 +0700

    add role organizer

--- utils/roles.ts
+++ utils/roles.ts
@@ -7,3 +7,13 @@ export const checkRole = (roles: Roles | Roles[]) => {
 
   return Array.isArray(roles) ? roles.includes(userRole) : userRole === roles;
 }
+
+export const isOrganizer = () => {
+  const { sessionClaims } = auth()
+  return sessionClaims?.metadata.role === 'organizer'
+}
+
+export const getRole = (): Roles | undefined => {
+  const { sessionClaims } = auth()
+  return sessionClaims?.metadata.role as Roles
+}
