e552093381eedbc858a83d5cb33ff5a0f877a0de
vanquy1410 <quynguyenpro1410@gmail.com>
2024-10-11 10:07:28 +0700

    fix role usertable

--- app/admin/dashboard/_actions.ts
+++ app/admin/dashboard/_actions.ts
@@ -6,18 +6,18 @@ import { clerkClient } from "@clerk/nextjs/server";
 export async function setRole(formData: FormData) {
   // Check that the user trying to set the role is an admin or employee
   if (!checkRole(["admin", "organizer"])) {
-    return { message: "Not Authorized" };
+    throw new Error("Not Authorized");
   }
 
   try {
-    const res = await clerkClient.users.updateUser(
+    await clerkClient.users.updateUser(
       formData.get("id") as string,
       {
         publicMetadata: { role: formData.get("role") },
       }
     );
-    return { message: res.publicMetadata };
   } catch (err) {
-    return { message: err };
+    console.error("Error updating user role:", err);
+    throw new Error("Failed to update user role");
   }
 }
