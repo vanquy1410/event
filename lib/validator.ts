8fdfe91769bd9d8fadf70436bb86e58225b7b8ec
s1jming <77600026+nhangian2017@users.noreply.github.com>
2024-12-09 10:07:14 +0700

    fix loi tao su kien

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,15 +1,15 @@
 import * as z from "zod"
 
 export const eventFormSchema = z.object({
-  title: z.string().min(3, 'Title must be at least 3 characters'),
-  description: z.string().min(3, 'Description must be at least 3 characters'),
-  location: z.string().min(3, 'Location must be at least 3 characters'),
-  imageUrl: z.string().min(1, 'Image is required'),
+  title: z.string().min(3, 'Tiêu đề phải có ít nhất 3 kí tự'),
+  description: z.string().min(20, 'Mô tả phải có ít nhất 20 ký tự'),
+  location: z.string().min(3, 'Địa điểm phải có ít nhất 3 kí tự'),
+  imageUrl: z.string().min(1, 'Phải có hình ảnh'),
   startDateTime: z.date(),
   endDateTime: z.date(),
   categoryId: z.string(),
   price: z.string(),
   isFree: z.boolean(),
   url: z.string(),
-  participantLimit: z.number().min(1, 'Participant limit must be at least 1'),
+  participantLimit: z.number().min(1, 'Người tham gia ít nhất phải là 1'),
 })
